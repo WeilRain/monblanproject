c4784b10512974b8d88b04263a7953ade5ed6432
Weil_Rain <alexey.krivchak@gmail.com>
2026-06-01 19:35:09 +0300

    fix: update image paths to use relative paths in mock data

--- src/mockData.js
+++ src/mockData.js
@@ -1,7 +1,7 @@
 export const profile = {
   name: 'monblanproject',
   started: '2024-01-01',
-  avatar: '/src/assets/images/logo.svg',
+  avatar: '../assets/images/logo.svg',
 }
 
 export const stats = {
@@ -13,63 +13,63 @@ export const stats = {
 export const posts = [
  {
     id: 1,
-    imageUrl: '/src/assets/images/Rectangle 20.svg', 
+    imageUrl: '../assets/images/Rectangle 20.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 2,
-    imageUrl: '/src/assets/images/Rectangle 20-2.svg', 
+    imageUrl: '../assets/images/Rectangle 20-2.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 3,
-    imageUrl: '/src/assets/images/Rectangle 20-3.svg',
+    imageUrl: '../assets/images/Rectangle 20-3.svg',
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 4,
-    imageUrl: '/src/assets/images/rectangle 20-4.svg', 
+    imageUrl: '../assets/images/rectangle 20-4.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 5,
-    imageUrl: '/src/assets/images/rectangle 20-5.svg', 
+    imageUrl: '../assets/images/rectangle 20-5.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 6,
-    imageUrl: '/src/assets/images/rectangle 20-6.svg', 
+    imageUrl: '../assets/images/rectangle 20-6.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 7,
-    imageUrl: '/src/assets/images/rectangle 20-7.svg', 
+    imageUrl: '../assets/images/rectangle 20-7.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 8,
-    imageUrl: '/src/assets/images/rectangle 20-5.svg', 
+    imageUrl: '../assets/images/rectangle 20-5.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 9,
-    imageUrl: '/src/assets/images/rectangle 20-4.svg',
+    imageUrl: '../assets/images/rectangle 20-4.svg',
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
