b8aeaa1e0e86cad17999356be12d52715137d12d
Weil_Rain <alexey.krivchak@gmail.com>
2026-06-01 19:57:25 +0300

    test5

--- src/mockData.js
+++ src/mockData.js
@@ -13,63 +13,63 @@ export const stats = {
 export const posts = [
  {
     id: 1,
-    imageUrl: '/images/Rectangle 20.svg', 
+    imageUrl: '../assets/images/rectangle 20.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 2,
-    imageUrl: '/images/Rectangle 20-2.svg', 
+    imageUrl: '../assets/images/rectangle 20-2.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 3,
-    imageUrl: '/images/Rectangle 20-3.svg',
+    imageUrl: '../assets/images/rectangle 20-3.svg',
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 4,
-    imageUrl: '/images/rectangle 20-4.svg', 
+    imageUrl: '../assets/images/rectangle 20-4.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 5,
-    imageUrl: '/images/rectangle 20-5.svg', 
+    imageUrl: '../assets/images/rectangle 20-5.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 6,
-    imageUrl: '/images/rectangle 20-6.svg', 
+    imageUrl: '../assets/images/rectangle 20-6.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 7,
-    imageUrl: '/images/rectangle 20-7.svg', 
+    imageUrl: '../assets/images/rectangle 20-7.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 8,
-    imageUrl: '/images/rectangle 20-5.svg', 
+    imageUrl: '../assets/images/rectangle 20-5.svg', 
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
   },
   {
     id: 9,
-    imageUrl: '/images/rectangle 20-4.svg',
+    imageUrl: '../assets/images/rectangle 20-4.svg',
     uploadDate: '11-04-2016',
     metricsToday: { likes: 128, comments: 31 },
     metricsHistory: { date: '9-08-2016', likes: 67, comments: 22 }
